f47861b1df6bb83c2375e3ea8734436f785066a5
Yuliia <kotyuliya24@gmail.com>
2024-05-07 13:55:17 -0400

    done

---
+++ src/js/2-form.js
@@ -0,0 +1,54 @@
+'use strict';
+
+const form = document.querySelector(".feedback-form");
+const errorMessage = document.createElement("p");
+errorMessage.classList.add("error-message");
+errorMessage.textContent = "Fill please all fields";
+errorMessage.style.display = "none";
+
+form.appendChild(errorMessage);
+
+const formData = {
+  email: "",
+  message: ""
+};
+
+form.addEventListener("input", function (event) {
+  if (event.target.name === "email") {
+    formData.email = event.target.value.trim();
+  } else if (event.target.name === "message") {
+    formData.message = event.target.value.trim();
+  }
+
+  localStorage.setItem("feedback-form-state", JSON.stringify(formData));
+
+});
+
+const savedData = localStorage.getItem("feedback-form-state");
+if (savedData !== null) {
+  const parsedData = JSON.parse(savedData);
+  formData.email = parsedData.email;
+  formData.message = parsedData.message;
+}
+
+form.addEventListener("submit", handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const formFields = event.target;
+
+  if (!formData.email || !formData.message) {
+    errorMessage.style.display = "block";
+    return;
+  }
+
+  console.log(formData);
+  formFields.reset();
+  localStorage.clear();
+
+  formData.email = "";
+  formData.message = "";
+  
+  errorMessage.style.display = "none";
+}
+
